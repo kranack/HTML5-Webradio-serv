8409e977a507a6932544b5810906bf14d47459e1
Damien Calesse <madeinjb@gmail.com>
2014-07-05 21:24:49 +0200

    Fuck

--- lib/onAir.php
+++ lib/onAir.php
@@ -9,28 +9,25 @@
 
 require_once ("./functions.php");
 require_once ("./Icecast.php");
+include_once ("../vendor/simplehtmldom/simple_html_dom.php");
+
+function __autoload($classname) {
+    $filename = "./modules/". $classname .".class.php";
+    include_once($filename);
+}
 
 if (isset($_GET) && ($_GET['server'])) {
 
 	$serveur = new IceCast();
 	$html = "";
 	$infos = array();
-
-	switch ($_GET['server']) {
-		case 'ouifm' :
+	
+	switch ($_GET['address']) {
+		case '':
 			$serveur->setUrl($_GET['address']);
 			$tmp = $serveur->getStatus();
-			$infos = getRadioInfos("oui", $tmp);
-			break;
-		case 'mouv' :
-			$serveur->setUrl($_GET['address']);
-			$tmp = $serveur->getStatus();
-			$infos = getRadioInfos("mouv", $tmp);
-			break;
-		case 'fip' :
-			$serveur->setUrl($_GET['address']);
-			$tmp = $serveur->getStatus();
-			$infos = getRadioInfos("fip", $tmp);
+			echo 'no address';
+			//$infos = $_GET['server']::getInfos($tmp);
 			break;
 		default :
 			$serveur->setUrl($_GET['address']);
