8ac1559f8f2cdc2c658d5345e022d53ec1e5c0fe
Damien Calesse <madeinjb@gmail.com>
2014-07-07 15:17:51 +0200

    Add Modules
    Modules : France Inter & France Musique

---
+++ lib/modules/Franceinter.class.php
@@ -0,0 +1,15 @@
+<?php
+
+class Franceinter {
+
+
+	public static function getInfos($tmp) {
+		$infos = $tmp;
+		
+		$infos['title'] = 'France Inter';
+		
+
+		return $infos;
+	}
+
+}
---
+++ lib/modules/Francemusique.class.php
@@ -0,0 +1,15 @@
+<?php
+
+class Francemusique {
+
+
+	public static function getInfos($tmp) {
+		$infos = $tmp;
+		
+		$infos['title'] = 'France Musique';
+		
+
+		return $infos;
+	}
+
+}
