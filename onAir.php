53c31d785c69445cb32fdf7accaa3c2367ec0dd4
Damien Calesse <madeinjb@gmail.com>
2014-07-29 14:12:38 +0200

    Organize lib

--- onAir.php
+++ onAir.php
@@ -8,15 +8,7 @@
  *
  ************************************/
 
-define('LIB_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR);
-define('VENDOR_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR);
-define('MODULE_PATH', LIB_PATH . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR);
-
-
-require_once (LIB_PATH . 'Icecast/Icecast.php');
-require_once (VENDOR_PATH . 'simplehtmldom/simple_html_dom.php');
-require_once (LIB_PATH . 'autoload.php');
-require_once (LIB_PATH . 'server.class.php');
+require_once ('./require.php');
 
 
 if (isset($_POST) && ($_POST['server'])) {
